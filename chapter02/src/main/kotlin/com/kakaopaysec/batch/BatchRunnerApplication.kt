8a0a6646a4bcaa5b3ba6fb0438f80d29ba6f61f2
임준영 <a79007714@gmail.com>
2022-02-16 23:53:55 +0900

    chapter02 basic batch

--- chapter02/src/main/kotlin/com/kakaopaysec/batch/BatchRunnerApplication.kt
+++ chapter02/src/main/kotlin/com/kakaopaysec/batch/BatchRunnerApplication.kt
@@ -1,11 +1,48 @@
 package com.kakaopaysec.batch
 
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
+@EnableBatchProcessing
 @SpringBootApplication
-class BatchRunnerApplication
+class BatchRunnerApplication(
+    @Autowired private val jobBuilderFactory: JobBuilderFactory,
+    @Autowired private val stepBuilderFactory: StepBuilderFactory
+) {
+
+    @Bean
+    fun step(): Step {
+        return this.stepBuilderFactory.get("step1")
+            .tasklet(Tasklet { contribution, chunkContext ->
+                println("Hello, World")
+                RepeatStatus.FINISHED
+            }).build()
+    }
+
+    @Bean
+    fun job(): Job {
+        return this.jobBuilderFactory.get("job")
+            .start(step())
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<BatchRunnerApplication>(*args)
 }
+
+
+
+
+
+
+
